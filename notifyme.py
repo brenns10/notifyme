b648d0a8998219dc6c6c008b1453d247b5b83773
Stephen Brennan <smb196@case.edu>
2015-05-28 14:58:24 -0400

    Added full configuration support.

--- notifyme.py
+++ notifyme.py
@@ -16,7 +16,8 @@ There should be no extraneous whitespace.  The email address should include
 
 from contextlib import contextmanager
 from os.path import expanduser, expandvars
-from smtplib import SMTP_SSL
+from smtplib import SMTP, SMTP_SSL
+from configparser import ConfigParser
 
 import sys
 import socket
@@ -28,48 +29,89 @@ HOST = 'smtp.gmail.com'
 PORT = 465
 
 
+def NotifyMeMailer(object):
+    """Class that handles SMTP functions using the configuration file."""
+
+    def __init__(self, conf=CONF_FILE, profile='default'):
+        """
+        Create a mailer from a configuration file and profile.
+
+        :param conf: The location of the configuration file, which contains all
+        important values for opening an SMTP connection.
+        :param profile: Name of the profile (which is just a header in the
+        configuration file) to send from and to.
+        """
+        self._conf = conf
+        self._profile = profile
+
+        self._parse_config()
+
+    def _parse_config(self):
+        """Parse the configuration file!"""
+        parser = ConfigParser()
+        parser.read(self._conf)
+        profile = parser[self._profile]
+
+        self._email = profile['email']
+        self._hostname = profile['host']
+        self._username = profile.get('email', self._email)
+        self._password = profile['password']
+        self._port = int(profile['port'])
+        self._security = profile.get('security', 'none').upper()
+
+    def _get_smtp(self):
+        """Return the correct instance of SMTP for the required security."""
+        if self.security == 'SSL':
+            return SMTP_SSL(self._hostname, self._port)
+
+        smtp = SMTP(self._hostname, self._port)
+        if self.security == 'TLS':
+            smtp.starttls()
+        return smtp
+
+    def send(self, subject, body):
+        """Login, send a message to yourself, and exit."""
+        message = "From: %s\r\n"     \
+                  "To: %s\r\n"       \
+                  "Subject: %s\r\n"  \
+                  "\r\n"             \
+                  "%s" % (self._email, self._email, subject, body)
+
+        smtp = self._get_smtp()
+        smtp.login(self._username, self._password)
+        smtp.sendmail(self._email, self._email, message)
+        smtp.quit()
+
+
 @contextmanager
-def notify(task_name, conf=CONF_FILE):
-    """Context manager that sends an email notification on completion of body.
+def notify(task_name, conf=CONF_FILE, profile='default'):
+    """
+    Context manager that sends an email notification on completion of body.
 
     See module documentation for configuration.
 
     Parameters:
     - task_name: Name of the task to use in the email notification.
     - conf: Alternative config file.  Supports environment variables and ~.
-
     """
-    # Open the configuration and read it.
+    # Open the configuration and read it!
     config = expanduser(expandvars(conf))
-    with open(config, 'r') as f:
-        config_line = f.readline()
-    email, password = config_line.split(':', 1)
+    handler = NotifyMeMailer(conf=config, profile=profile)
 
     # Pass control to the caller.
     yield
 
-    # On exit, connect to SMTP server over SSL and send email alert.
-    smtp = SMTP_SSL(HOST, PORT)
-    smtp.login(email, password)
-
+    # Send the notification.
     subject = task_name + ' completed.'
-
-    message = "From: %s\r\n"    \
-              "To: %s\r\n"      \
-              "Subject: %s\r\n" \
-              "\r\n"            \
-              "%s" % (email, email, subject, subject)
-
-    smtp.sendmail(email, email, message)
-    smtp.quit()
+    handler.send(subject, subject)
 
 
 def _main():
-    """Main for CLI notification process.
+    """
+    Main for CLI notification process.
 
     Call notifyme.py with a command.  When the command completes, you will be
     emailed.
-
     """
 
     if not sys.argv[1:]:
